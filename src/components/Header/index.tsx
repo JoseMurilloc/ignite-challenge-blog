28c6f9d05949dafd763db76581fce197cfc727db
Jose Murillo <jooseemurillo@gmail.com>
2021-03-29 19:27:46 -0300

    🎉 End Challenge

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,16 @@
+import Link from 'next/link';
+import styles from './header.module.scss';
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export default function Header() {
-  // TODO
+  return (
+    <header className={styles.containerHeader}>
+      <Link href="/">
+        <a>
+          <img src="/images/logo.svg" alt="logo" />
+        </a>
+      </Link>
+    </header>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
 import { GetStaticProps } from 'next';
 
+import { FiCalendar, FiUser } from 'react-icons/fi';
+import Link from 'next/link';
+import Prismic from '@prismicio/client';
+import { useState } from 'react';
+import ApiServiceResponse from '@prismicio/client/types/ApiSearchResponse';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import Header from '../components/Header';
 
 interface Post {
   uid?: string;
@@ -24,13 +32,82 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export default function Home({ postsPagination }: HomeProps) {
+  const [posts, setPosts] = useState<Post[]>(postsPagination.results);
+  const [hasMorePosts, setHasMorePosts] = useState(!!postsPagination.next_page);
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+  async function handleLoadMorePosts(): Promise<void> {
+    const loadMorePostsResponse: ApiServiceResponse = await (
+      await fetch(postsPagination.next_page)
+    ).json();
 
-//   // TODO
-// };
+    setPosts(oldPosts => [...oldPosts, ...loadMorePostsResponse.results]);
+    setHasMorePosts(!!loadMorePostsResponse.next_page);
+  }
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <Header />
+
+        <main>
+          <ul>
+            {posts.map(post => (
+              <li className={styles.post}>
+                <Link href={`/post/${post.uid}`}>
+                  <a>
+                    <h1>{post.data.title}</h1>
+                    <p>{post.data.subtitle}</p>
+                    <div className={styles.infor}>
+                      <span>
+                        <FiCalendar size={20} color="#BBBBBB" />
+                        {format(
+                          new Date(post.first_publication_date),
+                          'dd MMM yyyy',
+                          {
+                            locale: ptBR,
+                          }
+                        )}
+                      </span>
+                      <span>
+                        <FiUser size={20} color="#BBBBBB" />
+                        {post.data.author}
+                      </span>
+                    </div>
+                  </a>
+                </Link>
+              </li>
+            ))}
+          </ul>
+
+          {hasMorePosts && (
+            <button
+              type="button"
+              onClick={handleLoadMorePosts}
+              className={styles.loadMorePosts}
+            >
+              Carregar mais posts
+            </button>
+          )}
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(
+    Prismic.predicates.at('document.type', 'blog'),
+    {
+      pageSize: 1,
+    }
+  );
+
+  return {
+    props: {
+      postsPagination: postsResponse,
+    },
+  };
+};
